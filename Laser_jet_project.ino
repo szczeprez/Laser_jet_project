7ed05565c5e6a727ea39aa1fb64a5a414b8c58be
szczeprez <szczeprez@poczta.fm>
2019-04-01 23:21:46 +0200

    add time scheduler

--- Laser_jet_project.ino
+++ Laser_jet_project.ino
@@ -4,15 +4,15 @@
 #define S3 7
 #define sensorOut 8
 #define laser 10
-#define button_time 11
-#define button_select_time 12
+#define button_time 9
+#define button_select_time 11
 
 int START = 0;
 int red = 0;
 int green = 0;
 int blue = 0;
 int SENSOR_DELAY = 100;
-int counterBreak = 0, state = 1;
+int counterBreak = 0, state = 0;
 uint32_t startCounterTime;
 double finishCounterTime = 0.0;
 unsigned long aktualnyCzas = 0;
@@ -22,7 +22,7 @@ unsigned long aktualnyCzasSENSOR = 0;
 unsigned long zapamietanyCzasSENSOR_Red = 0;
 unsigned long zapamietanyCzasSENSOR_Green = 0;
 unsigned long zapamietanyCzasSENSOR_Blue = 0;
-unsigned long listOfResults[10];
+long listOfResults[5] = {0};
 
 void setup() {
   pinMode(S0, OUTPUT);
@@ -30,8 +30,8 @@ void setup() {
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
   pinMode(sensorOut, INPUT);
-  pinMode(button_time, INPUT); 
-  pinMode(button_select_time, INPUT);
+  pinMode(button_time, INPUT_PULLUP);
+  pinMode(button_select_time, INPUT_PULLUP);
 
   pinMode(laser, OUTPUT);
 
@@ -52,11 +52,11 @@ void loop() {
     digitalWrite(S2, LOW);
     digitalWrite(S3, LOW);
     red = pulseIn(sensorOut, LOW);
-  //  Serial.println("Red");
+    //  Serial.println("Red");
 
   }
-  //delay(100); 
- 
+  delay(100);
+
   if (aktualnyCzasSENSOR - zapamietanyCzasSENSOR_Green >= 300UL) {
     zapamietanyCzasSENSOR_Green = aktualnyCzasSENSOR;
     digitalWrite(S2, HIGH);
@@ -64,37 +64,41 @@ void loop() {
     green = pulseIn(sensorOut, LOW);
     //Serial.println("Green");
   }
-//delay(100);
+  delay(100);
 
   if (aktualnyCzasSENSOR - zapamietanyCzasSENSOR_Blue >= 300UL) {
     zapamietanyCzasSENSOR_Blue = aktualnyCzasSENSOR;
     digitalWrite(S2, LOW);
     digitalWrite(S3, HIGH);
     blue = pulseIn(sensorOut, LOW);
- //   Serial.println("Blue");
+    //   Serial.println("Blue");
   }
-//delay(100);
+  delay(100);
   breakLaser();
 }
 
 void breakLaser() {
-int i;
+  int i;
   switch (state) {
     case 0:
 
-//button_start_time_state - startuj kolejny czas
-//button_select_time_state - przechodz po liscie czasów ktore aktualnie zostaly policzone
+      //button_start_time_state - startuj kolejny czas
+      //button_select_time_state - przechodz po liscie czasów ktore aktualnie zostaly policzone
 
-      Serial.println("Press the Button to Start!");
-      Serial.println("Display list of results!");
-      if(digitalRead(button_time) == 1){
-      i = 0;
+      Serial.println("Press the Button to Start! or display List");
+
+      if (digitalRead(button_time) == LOW) {
+        Serial.println("Pressed Button time 11111111 <<<<<<<<<");
+        //numberOfBreaks;
+        i = 0;
         state = 1;
 
-
         // Display list of results
-      } else if(digitalRead(button_select_time) == 1){
+      } else if (digitalRead(button_select_time) == LOW && sizeof(listOfResults) != 0) {
+        Serial.println("Pressed Button select time 222222 <<<<<<<<<");
+
         state = 4;
+
       }
       break;
 
@@ -109,19 +113,16 @@ int i;
       break;
 
     case 2:
-      Serial.println("Waiting for move ! ");
-      if (countLaserBreak(5)) {
-        Serial.print("CountetrBreak: ");
-        Serial.println(counterBreak);
+      odmierzamCzas(); 
+      if (countLaserBreak(2)) {
         state = 3;
       }
       break;
 
     case 3:
       Serial.println("Finished! ");
-      listOfResults[i] = finishTime() / 1000;
-      Serial.print(listOfResults[i]);
-      Serial.print(" s.");
+      // double val = finishTime() / 1000;
+      listOfResults[i] = finishTime();
 
       state = 0;
 
@@ -129,10 +130,10 @@ int i;
 
     case 4:
       Serial.println("Results: ");
-      for(int i = 0; i < listOfResults.length; i++){
-        Serial.print(listOfResults[i]);
+      for (int z = 0; z < sizeof(listOfResults); z++) {
+        Serial.println(listOfResults[z]);
       }
-      state = 1;
+      state = 0;
       break;
 
     default:
@@ -142,27 +143,31 @@ int i;
 }
 
 boolean countLaserBreak(uint8_t number) {
-  aktualnyCzas = millis();
-  roznicaCzasu = aktualnyCzas - zapamietanyCzas;
-
-  if (roznicaCzasu >= 1000UL) {
-    zapamietanyCzas = aktualnyCzas;
-    Serial.println(aktualnyCzas / 1000);
-  }
-
   if (red > 150 || green > 150 || blue > 150) {
     counterBreak++;
-    Serial.print("Linia przekroczona: "); 
-    Serial.print(counterBreak); 
-    Serial.println(" razy."); 
+    Serial.print("Linia przekroczona: ");
+    Serial.print(counterBreak);
+    Serial.println(" razy.");
   }
   if (counterBreak > number ) {
+    counterBreak = 0;
     return true;
   }
   return false;
 }
 void startCzas() {
   startCounterTime = millis();
+  zapamietanyCzas = 0, roznicaCzasu = 0; 
+}
+void odmierzamCzas() {
+  long actualTime = millis();
+  roznicaCzasu = actualTime - zapamietanyCzas;
+
+  if (roznicaCzasu >= 1000UL) {
+    zapamietanyCzas = actualTime;
+    Serial.print(actualTime / 1000);
+    Serial.println(" s.");
+  }
 }
 
 double finishTime() {
